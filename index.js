c2f1b0b67787a09d1daf3a6d72c1fa7c1ce720da
Placeblock <felix.weglehner@gmx.de>
2024-03-04 17:28:54 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = new express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(80 || 3000, ()=>{console.log("server is up")} )
+
